f7498a96cb818b8316f6cba7f7b0886d2b9541cf
Omkar Rana <omra21cs@cmrit.ac.in>
2025-04-27 22:45:28 +0530

    change

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -16,7 +13,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+// Increase the request body size limit
+app.use(express.json({ limit: "10mb" })); // Set a higher limit (e.g., 10mb)
 app.use(cookieParser());
 app.use(
   cors({
